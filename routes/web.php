cbf2aff45418a5f5ee8c8f8655897c3eab3a4588
SelePai99 <144569516+SelePai99@users.noreply.github.com>
2024-05-08 23:54:27 -0500

    categorias

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $categorias =['all','categoria1','categoria2','categoria3','categoria4','categoria5'];
+$name="sele";
+
+    //return view('welcome',['categorias'=>$categorias]);
+    return view('welcome',compact('categorias','name'));
 });
 
 Auth::routes();
