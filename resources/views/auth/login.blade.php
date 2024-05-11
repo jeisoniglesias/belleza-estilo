1765faa2200e45d48973f44dfff92541a82b668b
SelePai99 <144569516+SelePai99@users.noreply.github.com>
2024-05-11 16:31:11 -0500

    login-blade-register-blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,14 @@
 @section('content_auth')
     <div class="mt-5">
         <p class="mb-1 h-1">Iniciar sesion</p>
-        <p class="text-muted mb-2">Hola inicia.</p>
         <div class="d-flex flex-column ">
             <div class="container">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
-                    <div class="row mb-1 w-100 bg-danger py-2">
-                        <input type="text" class="rounded form-control w-75 mx-auto">
-                    </div>
-                    <div class="row mb-1">
-                        <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                    <div class="row mb-2 justify-content-center align-items-center ">
+                        <div class="col-md-9">
+                            <input placeholder=" Correo electrónico" type="text" tabindex="1" required autofocus
+                                id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                                 name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                             @error('email')
@@ -25,14 +20,11 @@
                             @enderror
                         </div>
                     </div>
-
-                    <div class="row mb-3">
-                        <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="password" type="password"
-                                class="form-control @error('password') is-invalid @enderror" name="password" required
-                                autocomplete="current-password">
+                    <div class="row mb-2 justify-content-center align-items-center">
+                        <div class="col-md-9">
+                            <input  placeholder=" Contraseña" type="password" tabindex="1" required autofocus id="password"
+                                type="password" class="form-control @error('password') is-invalid @enderror" name="password"
+                                required autocomplete="current-password">
 
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -41,7 +33,6 @@
                             @enderror
                         </div>
                     </div>
-
                     <div class="row mb-3">
                         <div class="col-md-6 offset-md-4">
                             <div class="form-check">
@@ -54,9 +45,8 @@
                             </div>
                         </div>
                     </div>
-
                     <div class="row mb-0">
-                        <div class="col-md-8 offset-md-4">
+                        <div class="col-md-6 offset-md-3">
                             <button type="submit" class="btn btn-black">
                                 {{ __('Login') }}
                             </button>
@@ -75,8 +65,7 @@
             <div class="mt-3">
                 <p class="mb-0 text-muted">Aun no tienes cuenta?</p>
                 <a class="btn btn-primary" href="{{ route('register') }}">Registrate
-                    <span class="fas fa-chevron-right ms-1">
-                    </span>
+                    <span class="fas fa-chevron-right ms-1"></span>
                 </a>
             </div>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,18 +2,68 @@
 
 @section('content_auth')
     <div class="mt-5">
-        <p class="mb-1 h-1">Registrarse.</p>
-        <p class="text-muted mb-2">Hola.</p>
-        <div class="d-flex flex-column ">
+        <p class="mb-1 h-1">Registrate</p>
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="row mb-3 justify-content-center align-items-center">
+                {{--<label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>--}}
 
+                <div class="col-md-9">
+                    <input placeholder="Nombre" type="text" tabindex="1" required autofocus id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                        value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-            <div class="mt-3">
-                <p class="mb-0 text-muted">Tienes cuenta?</p>
-                <a class="btn btn-primary" href="{{ route('login') }}">Inicia sesion
-                    <span class="fas fa-chevron-right ms-1">
-                    </span>
-                </a>
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
             </div>
+            <div class="row mb-3 justify-content-center align-items-center">                
+                <div class="col-md-9">
+                    <input placeholder="Correo electrónico" type="text" tabindex="1" required autofocus id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" 
+                        value="{{ old('email') }}" required autocomplete="email">
+
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <div class="row mb-3 justify-content-center align-items-center ">  
+                <div class="col-md-9">
+                    <input placeholder="Contraseña" type="password" tabindex="1" required autofocus  id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                        name="password" required autocomplete="new-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <div class="row mb-2 justify-content-center align-items-center">
+
+                <div class="col-md-9">
+                    <input  placeholder="Contraseña" type="password" tabindex="1" required autofocus id="password-confirm" type="password" class="form-control" name="password_confirmation" required
+                        autocomplete="new-password">
+                </div>
+            </div>
+            <div class="row mb-0">
+                <div class="col-md-6 offset-md-4 ">
+                    <button  type="submit" class="btn bg-pink-100">
+                        {{ __('Register') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+        <div class="mt-3">
+            <p class="mb-0 text-muted">Tienes cuenta?</p>
+            <a class="btn btn-primary" href="{{ route('login') }}">Inicia sesion
+                <span class="fas fa-chevron-right ms-1">
+                </span>
+            </a>
         </div>
+    </div>
     </div>
 @endsection
