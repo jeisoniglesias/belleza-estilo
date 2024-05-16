c277825a5ef54c5b313b197313dd605fb48dcf21
jeisoniglesias <101828895+jeisoniglesias@users.noreply.github.com>
2024-05-16 16:18:00 -0500

    formulario

--- resources/views/tipos/categoria/index.blade.php
+++ resources/views/tipos/categoria/index.blade.php
@@ -104,11 +104,11 @@
                 <p class="lead text-muted">Listado de categorias</p>
             </div>
             <div class="col-4">
-                <!--<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addProduct">Add Product</button>
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addProduct">Add Product</button>
+                <!--<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addProduct">Add Product</button>-->
                 <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
--->
-                @livewire('tipos.categoria-formulario' )
+
+
+                @livewire('tipos.category-form' )
             </div>
 
 
